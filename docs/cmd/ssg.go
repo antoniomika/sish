c6b345d0ea7f735b145daf5bda5dac8c2781554f
Eric Bower <me@erock.io>
2024-07-11 23:57:24 -0400

    docs: update pdocs
    docs: fixed link

--- docs/cmd/ssg.go
+++ docs/cmd/ssg.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log/slog"
+
 	"github.com/picosh/pdocs"
 )
 
@@ -14,74 +16,34 @@ func main() {
 				Text: "Getting Started",
 				Href: "/getting-started",
 				Page: pager("getting-started.md"),
-				Tag:  "Help",
-				Children: []*pdocs.Sitemap{
-					{Text: "Managed", Href: "#managed"},
-					{Text: "DNS", Href: "#dns"},
-					{Text: "Docker Compose", Href: "#docker-compose"},
-					{Text: "Docker", Href: "#docker"},
-					{Text: "Google Cloud Platform", Href: "#google-cloud-platform"},
-					{Text: "Authentication", Href: "#authentication"},
-				},
 			},
 			{
 				Text: "How it Works",
 				Href: "/how-it-works",
 				Page: pager("how-it-works.md"),
-				Tag:  "Help",
-				Children: []*pdocs.Sitemap{
-					{Text: "Port Forward", Href: "#port-forward"},
-					{Text: "Traditional VPN", Href: "#traditional-vpn"},
-					{Text: "sish Public", Href: "#sish-public"},
-					{Text: "sish Private", Href: "#sish-private"},
-				},
 			},
 			{
 				Text: "Forwarding Types",
 				Href: "/forwarding-types",
 				Page: pager("forwarding-types.md"),
-				Tag:  "Help",
-				Children: []*pdocs.Sitemap{
-					{Text: "HTTP", Href: "#href"},
-					{Text: "TCP", Href: "#tcp"},
-					{Text: "TCP Alias", Href: "#tcp-alias"},
-					{Text: "SNI", Href: "#sni"},
-				},
 			},
 			{
 				Text: "Cheatsheet",
 				Href: "/cheatsheet",
 				Page: pager("cheatsheet.md"),
-				Tag:  "Help",
-				Children: []*pdocs.Sitemap{
-					{Text: "Remote forward SSH tunnels", Href: "#remote-forward-ssh-tunnels"},
-					{Text: "Local forward SSH tunnels", Href: "#local-foward-ssh-tunnels"},
-					{Text: "HTTPS public access", Href: "#https-public-access"},
-					{Text: "HTTPS private access", Href: "#https-private-access"},
-					{Text: "Websocket", Href: "#websocket"},
-					{Text: "TCP public access", Href: "#tcp-public-access"},
-					{Text: "TCP private access", Href: "#tcp-private-access"},
-				},
 			},
-			{Text: "CLI", Href: "/cli", Page: pager("cli.md"), Tag: "CLI"},
+			{Text: "CLI", Href: "/cli", Page: pager("cli.md")},
 			{
 				Text: "Advanced",
 				Href: "/advanced",
 				Page: pager("advanced.md"),
-				Children: []*pdocs.Sitemap{
-					{Text: "Choose your own subdomain", Href: "#choose-your-own-subdomain"},
-					{Text: "Websocket Support", Href: "#websocket-support"},
-					{Text: "Allowlist IPs", Href: "#allowlist-ips"},
-					{Text: "Custom Domains", Href: "#custom-domains"},
-					{Text: "Load Balancing", Href: "#load-balancing"},
-				},
-				Tag: "Help",
 			},
-			{Text: "FAQ", Href: "/faq", Page: pager("faq.md"), Tag: "Help"},
+			{Text: "FAQ", Href: "/faq", Page: pager("faq.md")},
 		},
 	}
 
 	config := &pdocs.DocConfig{
+		Logger: slog.Default(),
 		Sitemap:  sitemap,
 		Out:      "./docs/public",
 		Tmpl:     "./docs/tmpl",
